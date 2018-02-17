a8de628066cc82ad8dfb104cae02c192232b3a5f
BrijeshParmar <bparmar360@gmail.com>
2018-02-17 22:46:13 +0530

    materialize_changes

--- ValidationPract/apps/ValidationPract/common/js/ValidationPract.js
+++ ValidationPract/apps/ValidationPract/common/js/ValidationPract.js
@@ -7,26 +7,23 @@
 
 function wlCommonInit(){
 
-	/*
-	 * Application is started in offline mode as defined by a connectOnStartup property in initOptions.js file.
-	 * In order to begin communicating with Worklight Server you need to either:
-	 * 
-	 * 1. Change connectOnStartup property in initOptions.js to true. 
-	 *    This will make Worklight framework automatically attempt to connect to Worklight Server as a part of application start-up.
-	 *    Keep in mind - this may increase application start-up time.
-	 *    
-	 * 2. Use WL.Client.connect() API once connectivity to a Worklight Server is required. 
-	 *    This API needs to be called only once, before any other WL.Client methods that communicate with the Worklight Server.
-	 *    Don't forget to specify and implement onSuccess and onFailure callback functions for WL.Client.connect(), e.g:
-	 *    
-	 *    WL.Client.connect({
-	 *    		onSuccess: onConnectSuccess,
-	 *    		onFailure: onConnectFailure
-	 *    });
-	 *     
-	 */
-	
-	
-	// Common initialization code goes here
+}
 
+function registerFunction(){
+	var email=document.getElementById("ruserid").value;
+	var name=document.getElementById("rname").value;
+	var age=document.getElementById("rage").value;
+	var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+	if (filter.test(email.value)) {
+		alert('Please provide a valid email address');
+		email.focus;
+	}
+	if(!(/^[A-Za-z ]+$/.test(name))&& name.length > 10){
+		alert('Please Provide Name');
+		name.focus;
+	}
+	if(age<18 || age>60){
+		alert('Please Provide Proper Age');
+		age.focus;
+	}
 }
